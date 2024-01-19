737467bbc939f002f7ca3b0e3694fe205cbee0a9
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2024-01-19 10:31:45 +0600

    footer section added

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { router } from './Routers/Routers'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <div className='max-w-screen-xl mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   </React.StrictMode>,
 )
