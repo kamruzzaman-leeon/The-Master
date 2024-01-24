1ff49d9a481820ac1a94a8844e237dee64ca92e0
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2024-01-24 13:12:23 +0600

    teach on front added.

--- src/Pages/TeachOn/TeachOn.jsx
+++ src/Pages/TeachOn/TeachOn.jsx
@@ -1,40 +1,73 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import useAuth from '../../hooks/useAuth';
-
+import useClasses from '../../hooks/useClasses';
+import Loading from '../../components/Loading/Loading'
 const TeachOn = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
+    const [isLoading, classes] = useClasses();
+    // console.log('teachon: ', classes)
+    if (isLoading) {
+        <Loading></Loading>
+    }
+    const ispending= false;
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
-    const onSubmit= data =>{
+    const onSubmit = data => {
         console.log(data);
+        // const reqTeacher = {
+            
+        // }
     }
     return (
         <div className=' min-h-screen'>
             <h2 className='text-4xl text-center'>teach on<span className='text-blue-600'>The Master!</span></h2>
-            <div className='mx-auto w-full md:max-w-screen-sm shadow-md'>
-                        <h2 className="text-5xl text-center  font-extrabold text-blue-600 my-5">Sign In Now!</h2>
-                        <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Name</span>
-                                </label>
-                                <input type="text" placeholder="Name" defaultValue={user.displayname} readOnly {...register('name')} className="input input-bordered" />
-                            
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <img src={user.photoURL}></img>                               
-                               
-                            </div>
-                           
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">submit</button>
-                            </div>
-                            
-                        </form>
+            <div className='mx-auto md:max-w-screen-md shadow-md'>
+
+                <form className="w-10/12 mx-auto card-body" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="avatar mx-auto">
+                        <div className="w-24 rounded">
+                            <img src={user?.photoURL} />
+                        </div>
                     </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <p>{user?.displayName}</p>
+                       
+
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Experience Level</span>
+                        </label>
+                        <select {...register('experience',{required:true})}>
+                        {/* <option defaultValue="">Select the Level</option> */}
+                            <option value={'beginner'}>Beginner</option>
+                            <option value={'experienced'}>Experienced</option>
+                            <option value={'someidea'}>Some Idea</option>
+                        </select>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <select {...register('category',{required:true})}>
+                        {/* <option defaultValue="">Select a category</option> */}
+                            {classes.map(item => (
+                                <option key={item._id} value={item.category}>
+                                    {item.category}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="form-control mt-6">
+                        <button disabled={ispending} className="btn btn-outline text-blue-600">submit for review</button>
+                    </div>
+
+                </form>
+            </div>
         </div>
     );
 };
